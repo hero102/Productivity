6f1a690649f675d798242a36b5009a5d0982bce3
hero102 <128199489+hero102@users.noreply.github.com>
2023-04-01 00:03:47 +0530

    Add files via upload

---
+++ myproject/Tasks/Music/js/music-list.js
@@ -0,0 +1,40 @@
+
+let allMusic = [
+  {
+    name: "Doremon",
+    artist: "lofi song",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "BUTTER",
+    artist: "BTS",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Parting of three crossing river",
+    artist: "Tale of the nine tailed",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Dynamite",
+    artist: "BTS Army",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Suzume no tojimari",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "SIYA_RAM",
+    artist: "RAM_SIYA",
+    img: "music-6",
+    src: "music-6"
+  },
+  
+];
